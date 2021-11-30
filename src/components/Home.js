f8360ca7d14d7562d4a6cd33e4818cd9c8f8592d
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-11-30 11:18:54 +0000

    Add dummy blog data using hooks state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new blog", body: "lorem ipsum...", author: "Kaisa", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "Daniel", id: 2 },
+    { title: "Breed tips", body: "lorem ipsum...", author: "Kaisa", id: 3 },
+  ]);
+
   return (
     <div className="home">
-      <h2>Home Page</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
