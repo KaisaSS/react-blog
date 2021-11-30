45c01cc4f6e219663330e7a468b6ef8c4ee3d163
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-11-30 20:27:04 +0000

    Post request to new blog posts

--- src/components/Create.js
+++ src/components/Create.js
@@ -4,11 +4,28 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("Kaisa");
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+    });
+  };
 
   return (
     <div className="create">
       <h2>Add a new blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog Title:</label>
         <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
         <label>Blog Body:</label>
@@ -18,7 +35,8 @@ const Create = () => {
           <option value="Kaisa">Kaisa</option>
           <option value="Daniel">Daniel</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
       </form>
     </div>
   );
