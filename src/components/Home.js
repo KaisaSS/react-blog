33638ae35a9bf565fc34dad4846763c49c7851e7
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-11-30 13:02:42 +0000

    Handle errors fetching data

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,20 +5,31 @@ import Loading from "./Loading";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        console.log(res);
+        if (!res.ok) {
+          throw Error("Could not fetch the data!");
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
         setIsLoading(false);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isLoading && <Loading />}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
