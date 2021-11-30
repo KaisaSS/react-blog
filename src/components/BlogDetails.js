9a1e501341329f3464aeb6e690bbecd60fa12e2a
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-11-30 17:04:38 +0000

    Fetch and display data for individual blog posts

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,11 +1,22 @@
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import Loading from "./Loading";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const { data, error, isLoading } = useFetch("http://localhost:8000/blogs/" + id);
 
   return (
     <div className="blog-details">
-      <h2>Blog details - {id} </h2>
+      {isLoading && <Loading />}
+      {error && <div>{error}</div>}
+      {data && (
+        <article>
+          <h2>{data.title}</h2>
+          <p>Written by {data.author}</p>
+          <div>{data.body}</div>
+        </article>
+      )}
     </div>
   );
 };
