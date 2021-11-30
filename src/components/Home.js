51a5b1407044837dd0e8b5dd4374f1ffe608fb69
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-11-30 12:52:34 +0000

    Add loading state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import Loading from "./Loading";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
@@ -11,10 +13,16 @@ const Home = () => {
       })
       .then((data) => {
         setBlogs(data);
+        setIsLoading(false);
       });
   }, []);
 
-  return <div className="home">{blogs && <BlogList blogs={blogs} title="All Blogs" />}</div>;
+  return (
+    <div className="home">
+      {isLoading && <Loading />}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+    </div>
+  );
 };
 
 export default Home;
