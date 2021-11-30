805c662675de69d1b7e2ff916360741ff8c62297
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-11-30 10:20:55 +0000

    Create Home component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { BrowserRouter } from "react-router-dom";
 
 //Components
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <div className="content"></div>
+        <div className="content">
+          <Home />
+        </div>
       </div>
     </BrowserRouter>
   );
