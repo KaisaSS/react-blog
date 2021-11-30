025579fe231c447c331df8ecbfa477ae70885b71
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-11-30 20:18:05 +0000

    Add state to form

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,14 +1,20 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Kaisa");
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
       <form>
         <label>Blog Title:</label>
-        <input type="text" required />
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
         <label>Blog Body:</label>
-        <textarea required></textarea>
+        <textarea value={body} onChange={(e) => setBody(e.target.value)} required></textarea>
         <label>Blog Author:</label>
-        <select>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="Kaisa">Kaisa</option>
           <option value="Daniel">Daniel</option>
         </select>
