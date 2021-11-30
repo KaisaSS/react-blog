2ff89df372a154d3288be331168619a17b066824
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-11-30 11:46:04 +0000

    Create delete blogs event

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,14 @@ const Home = () => {
     { title: "Breed tips", body: "lorem ipsum...", author: "Kaisa", id: 3 },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
 };
